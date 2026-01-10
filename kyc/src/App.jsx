5e2feed97b44b758abd4cdc5eb54382116725332
James Isaac <jamesisaac@Jamess-MacBook-Pro.local>
2026-01-10 12:49:26 -0800

    safe

--- kyc/src/App.jsx
+++ kyc/src/App.jsx
@@ -1,49 +1,70 @@
-import Index from "./pages/Index";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Profile from "./pages/Profile";
-import Deposit from "./pages/Deposit";
-import ChatPage from "./pages/ChatPage";
-import Withdraw from "./pages/Withdraw";
-import Dashboard from "./admin/Dashboard";
-import LoginAdmin from "./admin/LoginAdmin";
-import AdminChatPage from "./admin/AdminChatPage";
-import DashboardLayout from "./components/Nav/DashboardLayout";
+import React from 'react';
+import FaceID from './pages/profile/FaceID';
+import VerifyID from './pages/profile/VerifyID';
+import FaceIdDone from './pages/profile/FaceIdDone';
+import Verification from './pages/profile/Verification';
+import CameraScan from './pages/profile/CameraScan';
+import ChooseIdentity from './pages/profile/ChooseIdentity';
+import CameraSuccess from './pages/profile/CameraSuccess';
+import VerificationDone from './pages/profile/VerificationDone';
+import VerificationComfirm from './pages/profile/VerificationComfirm';
+import VerificationAdvance from './pages/profile/VerificationAdvance';
+import ChooseVerification from './pages/profile/ChooseVerification';
+import IdentityVerification from './pages/profile/IdentityVerification';
+import GoogleTranslate from './pages/utils/GoogleTranslate';
 
 import axios from 'axios';
 import { Toaster } from 'react-hot-toast';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css"
-
+import { useEffect } from 'react';
+import { UserContextProvider } from '../context/UserContext';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 axios.defaults.baseURL = 'https://chain-point-server.vercel.app';
 axios.defaults.withCredentials = true;
+//http://localhost:8080  
+import '../src/fonts/fonts.css';
+import '../src/fonts/font-icons.css';
+import '../src/css/bootstrap.min.css';
+import '../src/css/styles.css';
+import '../src/css/swiper-bundle.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-//http://localhost:8080 
+import { initializeSwiper } from "../src/js/carousel"
+
+import {
+  Preloader,
+} from "../src/pages/utils/Properties"
 
 function App() {
+  useEffect(() => {
+    initializeSwiper();
+    Preloader();
+  }, [])
 
   return (
     <>
-    <Toaster position='top-right' toastOptions={{ duration: 4000 }} />
-    <Router>
-      <Routes>
-        <Route index="/" element={<Index />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/admin" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/deposit" element={<Deposit />} />
-        <Route path="/withdraw" element={<Withdraw />} />
-        <Route path="/login-admin" element={<LoginAdmin />} />
-        <Route path="/dashboard" element={<DashboardLayout />} />
-        <Route path="/admin-chat-page" element={<AdminChatPage />} />
-      </Routes>
-    </Router>
-  </>
+      <UserContextProvider>
+        <Router>
+          <Routes>
+            <Route index element={<Verification />} />
+            <Route path='/FaceID' element={<FaceID />} />
+            <Route path='/FaceIdDone' element={<FaceIdDone />} />
+            <Route path='/VerifyID' element={<VerifyID />} />
+            <Route path='/CameraScan' element={<CameraScan />} />
+            <Route path='/Verification' element={<Verification />} />
+            <Route path='/CameraSuccess' element={<CameraSuccess />} />
+            <Route path='/ChooseIdentity' element={<ChooseIdentity />} />
+            <Route path='/VerificationDone' element={<VerificationDone />} />
+            <Route path='/VerificationComfirm' element={<VerificationComfirm />} />
+            <Route path='/VerificationAdvance' element={<VerificationAdvance />} />
+            <Route path='/ChooseVerification' element={<ChooseVerification />} />
+            <Route path='/IdentityVerification' element={<IdentityVerification />} />
+          </Routes>
+        </Router>
+        <Toaster position='top-right' toastOptions={{ duration: 4000 }} />
+        <GoogleTranslate />
+      </UserContextProvider>
+    </>
   )
 }
 
--- kyc/src/main.jsx
+++ kyc/src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx';
-
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId="170268353832-0fn4qbgklemeb9s0o5elvi99ronia9ov.apps.googleusercontent.com">
   <StrictMode>
-    <TonConnectUIProvider manifestUrl="https://kapital-fluss.vercel.app/tonconnect-manifest.json">
-      <App />
-    </TonConnectUIProvider>
-  </StrictMode>,
+    <App />
+  </StrictMode> 
+  </GoogleOAuthProvider> 
 )
